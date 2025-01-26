4f499fe0942b33fe844cbf89f01938ef4fc89b30
Luke Hsiao <luke@hsiao.dev>
2025-01-26 00:02:00 -0700

    feat: enable sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://alumni.cottonwoodhigh.school',
 	integrations: [
 		starlight({
 			title: 'Cottonwood Alumni',
