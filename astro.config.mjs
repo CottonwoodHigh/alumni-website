435553984cb67b47ca97797bef9388ee7808d292
Luke Hsiao <luke@hsiao.dev>
2025-01-26 19:44:17 -0700

    feat: add site logo

--- astro.config.mjs
+++ astro.config.mjs
@@ -17,6 +17,9 @@ export default defineConfig({
 			editLink: {
 				baseUrl: 'https://github.com/lukehsiao/cottonwood-alumni/edit/main/',
 			},
+			logo: {
+				src: './src/assets/colt.png',
+			},
 			favicon: 'favicon.png',
 			plugins: [starlightImageZoom()],
 			sidebar: [
